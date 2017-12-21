9b9bda25bf43b38929f3b848d6db00ab07ce95a5
Rwitam Bandyopadhyay <mailtorwitam@gmail.com>
2017-12-21 10:58:45 +0530

    Create 621 - Secret Research.cpp

---
+++ 621 - Secret Research.cpp
@@ -0,0 +1,30 @@
+#include<cstdio>
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	int T;
+	string result;
+	scanf("%d", &T);
+	getline(cin, result);
+	while (T--) {
+		getline(cin, result);
+		if (!result.compare("1") || !result.compare("4") || !result.compare("78")) {
+			cout << "+";
+		}
+		else if (!result.compare(result.size()-2, 2, "35")) {
+			cout << "-";
+		}
+		else if (result[0] == '9' && result[result.size() - 1] == '4') {
+			cout << "*";
+		}
+		else if (!result.compare(0, 3, "190")) {
+			cout << "?";
+		}
+		cout << endl;
+	}
+	return 0;
+}
